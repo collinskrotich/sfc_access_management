6aaf51a2b00981c8f4eddb899223e1709ccae54c
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-08-10 09:18:18 +0300

    Added footer

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 "use client";
+import Footer from '@/components/Footer';
 import { Sidebar } from '@/components/Sidebar'
 
 export default function Home() {
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <div className='flex flex-col min-h-screen'>
+      <div className='flex-grow'>
+        <Sidebar />     
+      </div>
+
+      <footer className='flex justify-center items-center w-full h-24 border-t'>
+      <p>© 2023 - All Rights Reserved</p>
+      </footer>
+  </div>
   )
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex justify-center items-center w-full h-24 border-t'>
+      <p>© 2022 - All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer

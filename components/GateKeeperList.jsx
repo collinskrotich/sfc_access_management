b75d7aa2ed61fbbad69b7432e6704821dadc00c8
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-08-28 06:32:22 +0300

    Returned user api list call

--- components/GateKeeperList.jsx
+++ components/GateKeeperList.jsx
@@ -3,26 +3,50 @@ import RemoveBtn from './RemoveBtn'
 import {HiPencilAlt} from 'react-icons/hi'
 import Link from 'next/link'
 
-function GateKeeperList() {
+const getGateKeepers = async () => {
+    try{
+        const res = await fetch('http://localhost:3001/api/gatekeepers',
+        
+        {
+        cache: "no-store",
+        }
+        );
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch gatekeeeper!");
+        }
+        return res.json();
+        
+    }
+    catch(error) {
+        console.log("Error loading gatekeepers:", error);
+    }   
+}
+
+
+async function GateKeeperList() {
+
+    const { gatekeepers } = await getGateKeepers();
 
   return (
-    <>  
-    <div className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
+    <>
+    {gatekeepers.map((t) => (
+    <div key={t._id} className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
         <div>
-            <h2 className='font-bold'>Papa Shirandula</h2>
-            <h2>onDuty = Yes</h2>
-        </div>
-
+            <h2 className='font-bold'>{t.name}</h2>
+            <h2>On Duty: {t.onDuty ? 'Yes' : 'No'}</h2>
+            <h2>Thika data center</h2>
+            <h2>Created at: {t.createdAt}</h2>
+            </div>
         <div className='flex gap-2'>
             <RemoveBtn/>
-            <Link href={'/editGateKeeper/${t.id}'}>
+            <Link href={'/editGateKeeper/123'}>
                 <HiPencilAlt size={24}/>
             </Link>
             </div> 
     </div>
-  
+    ))}
     </>
-
   )
 }
 
--- components/UserList.jsx
+++ components/UserList.jsx
@@ -1,64 +1,45 @@
-"use client";
-
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import RemoveBtn from './RemoveBtn'
 import {HiPencilAlt} from 'react-icons/hi'
 import Link from 'next/link'
 
-// const getUsers = async () => {
-//     try{
-//         const res = await fetch('/api/users',
-//         {
-//         cache: "no-store",
-//         });
+const getUsers = async () => {
+    try{
+        const res = await fetch('http://localhost:3001/api/users',
+        {
+        cache: "no-store",
+        });
 
-//         if (!res.ok) {
-//             throw new Error("Failed to fetch users!");
-//         }
-//         return res.json();
-//     }
-//     catch(error) {
-//         console.log("Error loading users:", error);
-//     }   
-// }
+        if (!res.ok) {
+            throw new Error("Failed to fetch users!");
+        }
+        return res.json();
+    }
+    catch(error) {
+        console.log("Error loading users:", error);
+    }   
+}
 
 
-function UserList() {
+async function UserList() {
 
-    // const { rusers } = await getUsers();
-
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-      const fetchUsers = async () => {
-        const response = await fetch("/api/users");
-        const data = await response.json();
-        setUsers(data);
-      };
-  
-      fetchUsers();
-    }, []);
+    const { rusers } = await getUsers();
 
   return (
     <>
-    {users.map((user) => (
-    <div className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
-        
+    {rusers.map((t) => (
+    <div key={t._id} className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
         <div>
-            <h2 className='font-bold' key={user.id}>{user.fullname}</h2>
-            <h2>{user.email}</h2>
-            <h2>{user.role}</h2>
+            <h2 className='font-bold'>{t.fullname}</h2>
+            <h2>{t.email}</h2>
+            <h2>{t.role}</h2>
         </div>
-
         <div className='flex gap-2'>
-
             <RemoveBtn/>
             <Link href={'/editUser/123'}>
                 <HiPencilAlt size={24}/>
             </Link>
-
-        </div> 
-
+            </div> 
     </div>
     ))}
     </>
